c76f75a706debaf0ad4ddd5d21cbfebd9f23712b
Matthew Bland <48534933+mabla0531@users.noreply.github.com>
2020-03-03 12:04:15 -0600

    Added first commit
    No ticking when minimized, window shows, polls events, list of things was made

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+/*
+ * The Alpha Chronicles, made in 2020 by Matthew Bland
+ * 
+ * This is my source code. There are many like it,
+ * but this one is mine. My source code is my friend. 
+ * It is my life. I  must master it as I master my life.
+ * Without me, my source code is useless. Without my
+ * source code, I am useless. I must write my source
+ * code true. I must write more concisely than my enemy
+ * who is trying to outperform me. I must write code
+ * before he writes code. I WILL. 
+ */
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+SDL_Window* window;
+bool running = false, minimized = false;
+SDL_Event event;
+
+void handleEvents() {
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            running = false;
+        } else if (event.window.event == SDL_WINDOWEVENT_MINIMIZED) {
+            minimized = true;
+        } else if (event.window.event == SDL_WINDOWEVENT_RESTORED) {
+            minimized = false;
+        }
+    }
+}
+
+void tick() {
+    if (!minimized) {
+        std::cout << "tick" << std::endl;
+    }
+}
+
+void render() {
+
+}
+
+void gameLoop() {
+    while (running) {
+        handleEvents();
+        tick();
+        render();
+    }
+}
+
+int main(int args, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("The Alpha Chronicles", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+
+    running = true;
+    gameLoop();
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
