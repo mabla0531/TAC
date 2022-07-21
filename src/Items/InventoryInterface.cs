f25448195769e042499713ca1b8c3ee200994b77
Matthew Bland <mabla0531@gmail.com>
2022-07-21 09:07:29 -0500

    Inventory Interface renders items in inventory

--- src/Items/InventoryInterface.cs
+++ src/Items/InventoryInterface.cs
@@ -5,19 +5,29 @@ using System;
 namespace TAC {
     class InventoryInterface {
         
-        private Vector2f Position;
         private Inventory inventory;
+
+        private Vector2f Position;
         private Sprite inventoryBG;
+        private Vector2f[] squarePositions;
+
         public bool Active {get; set;}
         
         public InventoryInterface(Inventory i) {
             inventory = i;
 
-            inventoryBG = new Sprite(Assets.ui, new IntRect(434, 290, 128, 128));
-            inventoryBG.Scale = new Vector2f(2.0f, 2.0f);
-            inventoryBG.Position = new Vector2f((Game.displayWidth / 2) - (inventoryBG.TextureRect.Width), (Game.displayHeight / 2) - (inventoryBG.TextureRect.Height));
+            inventoryBG = new Sprite(Assets.ui, new IntRect(192, 292, 184, 184));
+            inventoryBG.Scale = new Vector2f(1.0f, 1.0f);
+            inventoryBG.Position = new Vector2f((Game.displayWidth / 2) - (inventoryBG.TextureRect.Width / 2), (Game.displayHeight / 2) - (inventoryBG.TextureRect.Height / 2));
 
             Active = false;
+
+            squarePositions = new Vector2f[25];
+            for (int y = 0; y < 5; y++) {
+                for (int x = 0; x < 5; x++) {
+                    squarePositions[(y * 5) + x] = new Vector2f(((x + 1) * 4) + (x * 32) + inventoryBG.Position.X, ((y + 1) * 4) + (y * 32) + inventoryBG.Position.Y);
+                }
+            }
         }
 
         public void tick() {
@@ -28,8 +38,12 @@ namespace TAC {
             if (!Active) return;
             
             window.Draw(inventoryBG);
-            foreach(Item i in inventory.Items) {
-                Console.WriteLine(i.Name);
+            Sprite currentSprite;
+            for (int i = 0; i < inventory.Items.Count; i++) {
+                currentSprite = new Sprite(inventory.Items[i].Icon);
+                currentSprite.Scale = new Vector2f(2.0f, 2.0f);
+                currentSprite.Position = squarePositions[i];
+                window.Draw(currentSprite);
             }
         }
     }
